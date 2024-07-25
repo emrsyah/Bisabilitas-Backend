3bdfac9765c5fd979089356c3d1aa56de4c30598
Emirsyah <muhammademir48@gmail.com>
2024-07-25 17:50:04 +0700

    feat: cleaning data

--- src/api/ai.ts
+++ src/api/ai.ts
@@ -1,40 +1,73 @@
 import express from 'express';
-import { PuppeteerWebBaseLoader } from '@langchain/community/document_loaders/web/puppeteer';
+import { PuppeteerWebBaseLoader, Page, Browser } from '@langchain/community/document_loaders/web/puppeteer';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { pull } from 'langchain/hub';
 import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 import { ChatMistralAI, MistralAIEmbeddings } from '@langchain/mistralai';
 import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { Document } from 'langchain/document';
 import { StringOutputParser } from '@langchain/core/output_parsers';
 import { createStuffDocumentsChain } from 'langchain/chains/combine_documents';
 import { HtmlToTextTransformer } from '@langchain/community/document_transformers/html_to_text';
+import { MozillaReadabilityTransformer } from '@langchain/community/document_transformers/mozilla_readability';
+import * as cheerio from 'cheerio';
 
 const router = express.Router();
 
 type EmojiResponse = string[];
 
 router.get<{}, EmojiResponse>('/', async (req, res) => {
-  const loader = new PuppeteerWebBaseLoader('https://docs.pinecone.io/reference/api/introduction');
+  const loader = new PuppeteerWebBaseLoader('https://js.langchain.com/v0.2/docs/integrations/document_loaders/web_loaders/web_cheerio', {
+    launchOptions: {
+      headless: 'new',
+    },
+    async evaluate(page: Page, browser: Browser) {
+      const textContent = await page.evaluate(() => {
+        const bodyElement = document.querySelector('body');
+        return bodyElement?.textContent ?? '' ;
+      });
+      await browser.close();
+      return textContent ?? '';
+    },
+  });
 
   const docs = await loader.load();
+  const pageContent = docs[0].pageContent;
+  const $ = cheerio.load(pageContent);  
+  $('script, style').remove();
+  const cleanedText = $('body').html()?.replace(/<style[^>]*>.*<\/style>/gms, '');
+  const cleaned$ = cheerio.load(cleanedText!);
+
+  const textContent = cleaned$('body').text();
+
+  const docsC = textContent.replace(/[^\x20-\x7E]+/g, '');
+
+  const documents =  [new Document( { pageContent: docsC } )];
+
+  console.log('docs', documents);
 
   const transformer = new HtmlToTextTransformer();
-
+  const transformer2 = new MozillaReadabilityTransformer();
 
   const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 1000,
-    chunkOverlap: 200,
+    chunkSize: 500,
+    chunkOverlap: 100,
   });
+
   // const transformer = new HtmlToTextTransformer();
-  const sequence  = splitter.pipe(transformer);
-  const split = await sequence.invoke(docs);
-  console.log('split length', split.length);
-  console.log('page content', split[0].pageContent.length);
-  console.log('meta 1', split[0].metadata);
-  console.log('meta 2', split[2].metadata);
+  const sequence  = splitter.pipe(transformer2);
+  const split = await sequence.invoke(documents);
+  // console.log('split length', split);
+  // console.log('split length', split.length);
+  // console.log('page content', split[10].pageContent);
+  // console.log('meta 1', split[0].metadata);
+  // console.log('meta 2', split[2].metadata);
+
   const vectorStore = await MemoryVectorStore.fromDocuments(
     split,
-    new MistralAIEmbeddings(),
+    new MistralAIEmbeddings({
+      // batchSize: 200000,
+    }),
   );
 
   console.log('berhasil lewat vector store');
@@ -52,7 +85,7 @@ router.get<{}, EmojiResponse>('/', async (req, res) => {
     outputParser: new StringOutputParser(),
   });
 
-  const retrievedDocs = await retriever.invoke('how i can doing aunthetication in pinecone?');
+  const retrievedDocs = await retriever.invoke('what project the owner build that won competition, and what is used for?');
 
   console.log('lenght context', retrievedDocs.length);
   console.log('context diambil id', retrievedDocs[0].id);
@@ -60,7 +93,7 @@ router.get<{}, EmojiResponse>('/', async (req, res) => {
   console.log('context diambil metadata', retrievedDocs[0].metadata);
 
   const coba = await ragChain.invoke({
-    question: 'how i can doing aunthetication in pinecone?',
+    question: 'what project the owner build that won competition, and what is used for?',
     context: retrievedDocs,
   });
 
